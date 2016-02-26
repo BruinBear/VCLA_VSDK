eb381df4492f1adaa818b9121e227e1cc3c5951d
Raymond <raymond12365@gmail.com>
2016-02-26 11:02:10 -0800

    remove env

--- server/queryserver/static/js/core/router.js
+++ server/queryserver/static/js/core/router.js
@@ -1,20 +1,24 @@
 define(function(require) {
 	var Backbone = require('Backbone');
 	var viewManager = require('./viewManager');
+	var queryView = require('./../apps/video/views/QueryView');
 
 	var Router = Backbone.Router.extend({
 		routes: {
 			'index/#': 'video',
 			'index/#contacts': 'contacts',
-			'index/#tasks': 'tasks'
+			'index/#tasks': 'tasks',
+			'index/#queries': 'queries',
 		},
 
 		video: function () {
+			this.view && this.view.remove();
 			require('./../apps/video/app').run(viewManager);
 		},
 
-		predicate: function () {
-			require('./../apps/contacts/app').run(viewManager);
+		queries: function () {
+			this.view && this.view.remove();
+			this.view = new queryView();
 		},
 
 		help: function () {
--- server/serverenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Raymond/Documents/VCLA/VCLA_VSDK/server/serverenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
